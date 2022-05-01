83d2add3fc9ff56b047295e3174ffc70adb425ac
Andrew Galkin <andrushka@Air-Andrew.fritz.box>
2022-05-01 13:37:52 +0200

    add albums

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../src/App.js";
 import "./index.css";
+import Albums from "./pages/Albums/Albums";
 
 render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/main" exact element={<App />} />
+        <Route path="/albums" exact element={<Albums />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
